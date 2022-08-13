a256a95c0760abc050c4e815260eb3d6a7b9989a
Derry Nixon <derry@plushcare.com>
2022-08-12 21:22:10 -0400

    getting started

---
+++ dragonball-lotto/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import { Box } from '@chakra-ui/react'
+
+const Home: NextPage = () => {
+  return (
+    <Box w='100%'>
+      <Box w='100%'>
+        Dragon Ball Lotto
+      </Box>
+    </Box>
+  );
+}
+
+export default Home
